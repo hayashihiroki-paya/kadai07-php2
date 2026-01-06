f7fd5ade864be91740bfbbb771454730f26e7902
hayashihiroki-paya <hayashihiroki517@gmail.com>
2026-01-06 18:59:13 +0900

    readme更新、一部関数に置き換え、不要なコメント削除

--- php/login.php
+++ php/login.php
@@ -43,39 +43,4 @@ foreach ($result as $record) {
         exit();
     }
 }
-
-
-// $file = fopen("../data/users.csv", "r");
-// flock($file, LOCK_SH);
-
-// if ($file) {
-
-//     while ($users = fgetcsv($file)) {
-
-//         // var_dump($line);
-
-//         if ($users[0] === $_POST["userID"]) {
-//             // echo "データベース：" . $users[0] . "POSTデータ" . $_POST["userID"] . "で一致しました";
-//             // 一致してたらセッションに保存
-//             $_SESSION["userID"] = $users[0];
-//             $_SESSION["userName"] = trim($users[1]);
-
-//             // ファイルを閉じて返す
-//             flock($file, LOCK_UN);
-//             fclose($file);
-
-//             header("Location: ../index.php");
-//             exit();
-//         } else {
-//             // echo "データベース：" . $users[0] . "POSTデータ" . $_POST["userID"] . "で不一致";
-//         }
-
-
-//     }
-// }
-
-// // 念のため 後々登録がなかった時の処理を作ったときに適切な位置に動かします
-// flock($file, LOCK_UN);
-// fclose($file);
-
 exit();
